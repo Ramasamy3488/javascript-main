8c17e00dbf6dd91cc6ae5342f79ab21141a9e0b1
Ramasamy3488@gmail.com <kvpramasamy@gmail.com>
2024-09-30 14:59:36 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,47 @@
+
+// function tiger(){
+//     document.getElementById('myforest').src='./tiger.webp'
+// }
+// function lion(){
+//     document.getElementById('myforest').src='./lion.jpg'
+// }
+// function cat(){
+//     document.getElementById('myforest').src='./cat.jfif'
+// }
+// function monkey(){
+//     document.getElementById('myforest').src='./monkey.jfif'
+// }
+// function dog(){
+//     document.getElementById('myforest').src='./dog.webp'
+
+// }
+// function forest(){
+//     document.getElementById('myforest').src='./forest.jfif'    
+// }
+
+
+
+// for(var i=0; i<arranimals.length;i++ )
+
+
+function animals() {
+    var arranimals = ['tiger.webp', 'lion.jpg', 'cat.jfif', 'monkey.jfif', 'forest.jfif']
+
+    
+   
+}
+
+
+
+
+
+// console.log(arranimals[0]);
+// console.log(arranimals[1]);
+// console.log(arranimals[2]);
+// console.log(arranimals[3]);
+// console.log(arranimals[4]);
+
+
+
+
+
